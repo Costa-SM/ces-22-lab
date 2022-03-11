6dccc1f4d366f00b3101f09ee8d1419ca51154f1
Arthur Stevenson <arthur-csm@hotmail.com>
2022-03-10 21:30:33 -0300

    Fixed extra space.

--- Lab1/3intsum.py
+++ Lab1/3intsum.py
@@ -1,11 +1,10 @@
-
 def sum_to(n):
     """
     :param n: sum upper limit
     :type n: int.
     :rtype: int.
     """
-    
+
     sum = 0
     for i in range(n):
         # range starts on integer 0, so we shift the sum by one
